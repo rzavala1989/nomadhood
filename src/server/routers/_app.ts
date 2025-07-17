bddba00a5cf7ac6d90795a79bac3cac79abd149e
Ricardo Zavala <zavala.ricardo.m@gmail.com>
2025-07-17 21:03:34 +0000

    feat: integrate reviews router into main app router

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -5,11 +5,13 @@ import { createCallerFactory, publicProcedure, router } from '../trpc';
 import { userRouter } from './user';
 import { neighborhoodsRouter } from '@/server/routers/neighborhoodsRouter';
 import { favoritesRouter } from '@/server/routers/favoritesRouter';
+import { reviewsRouter } from '@/server/routers/reviewsRouter';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
   user: userRouter,
   neighborhoods: neighborhoodsRouter,
+  reviews: reviewsRouter,
   favorites: favoritesRouter,
 });
 
